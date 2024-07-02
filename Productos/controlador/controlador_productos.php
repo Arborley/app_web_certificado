857dd487655727e7ef788425bf94467c8494449f
Andrea Villegas <cursosycertificados99@gmail.com>
2024-07-02 20:47:57 +0200

    Update controlador_productos.php

--- Productos/controlador/controlador_productos.php
+++ Productos/controlador/controlador_productos.php
@@ -1,13 +1,33 @@
 <?php
-// Incluir el archivo de configuración
-require_once __DIR__ . '../../../config.php';
+require_once __DIR__ . '/../config.php';
+require_once MODEL_PATH . 'modelo_listar.php';
 
-// Incluir el modelo de contactos
-require_once PRODUCTOS_MODEL_PATH . '/modelo_productos.php';
-$action = $_GET['action'];
 
-switch($action) {
-    case'consultar':
-    consulta();
-    break;
+
+if (($_SERVER["REQUEST_METHOD"] == "POST")) {
+    $nombre = $descripcion = $precio = $impuesto= '';
+
+    if (isset($_POST['formulario'])) {
+        switch ($_POST['formulario']) {
+            case 'listar':
+                $nombre = limpiar_dato($_POST['nombre']);
+                $descripcion = limpiar_dato($_POST['descripcion']);
+                $precio = limpiar_dato($_POST['precio']);
+                $impuesto = limpiar_dato($_POST['impuesto']);
+
+                echo '<a href = "../vista/vista_listado_productos.php"></a>';
+            break;
+        }
+    }
 }
+
+//Función para limpiar el dato obtenido en el formulario
+function limpiar_dato($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+?>
